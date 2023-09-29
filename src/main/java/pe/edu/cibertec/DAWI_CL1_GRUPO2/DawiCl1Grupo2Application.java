70200ed239a2ab7890931e18fcbde33f26b8c958
chiszo <gigibethandrea@gmail.com>
2023-09-29 14:45:34 -0500

    Creando repositorio, agregando bootstrap y carpetas iniciales

---
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO2/DawiCl1Grupo2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL1_GRUPO2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1Grupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl1Grupo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWI_CL1_GRUPO2/DawiCl1Grupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWI_CL1_GRUPO2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1Grupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
