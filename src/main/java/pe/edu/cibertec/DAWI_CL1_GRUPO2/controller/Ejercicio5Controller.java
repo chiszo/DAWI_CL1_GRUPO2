77bbfd0c0c7e8aac514d2ea2874587e25a23b719
maria309isabel <isa.mar.xv@gmail.com>
2023-09-30 13:09:57 -0500

    prueba2

--- src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO2/controller/Ejercicio5Controller.java
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO2/controller/Ejercicio5Controller.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pe.edu.cibertec.DAWI_CL1_GRUPO2.model.Ejercicio5;
 
 
@@ -17,19 +18,21 @@ public class Ejercicio5Controller {
         return "ejercicio5";
     }
 
+    @PostMapping("/calcularprecio")
     public String calcularPrecio(@ModelAttribute("rebaja") Ejercicio5 ejercicio, Model model){
-        Double precioTotal;
-        if (ejercicio.getCantidadAdquirida() > 20) {
-            precioTotal = ejercicio.getPrecioUnitario() * ejercicio.getCantidadAdquirida() * 0.9;
-        } else if(ejercicio.getCantidadAdquirida() > 10) {
-            precioTotal = ejercicio.getPrecioUnitario() * ejercicio.getCantidadAdquirida() * 0.95;
+        Double precioTotal= ejercicio.getPrecioUnitario() * ejercicio.getCantidadAdquirida();
+        Integer cantidad = ejercicio.getCantidadAdquirida();
+        if (cantidad > 20) {
+            precioTotal = precioTotal * 0.9;
+        } else if(cantidad > 10) {
+            precioTotal = precioTotal * 0.95;
         } else {
-            precioTotal = ejercicio.getPrecioUnitario() * ejercicio.getCantidadAdquirida();
+            precioTotal = precioTotal;
         }
 
         model.addAttribute("verresultado",true);
-        model.addAttribute("resultado", "Precio total a pagar: "+ String.format("%.2f",precioTotal));
-        return "ejercicio1";
+        model.addAttribute("resultado", "Precio total a pagar: " +precioTotal);
+        return "ejercicio5";
     }
 
 }
