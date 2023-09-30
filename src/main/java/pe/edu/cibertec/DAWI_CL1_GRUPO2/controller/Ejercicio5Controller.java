654ec247ee6e9e056f9549685ca8a1eb0a6a114b
maria309isabel <isa.mar.xv@gmail.com>
2023-09-30 12:15:27 -0500

    commit

--- src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO2/controller/Ejercicio5Controller.java
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO2/controller/Ejercicio5Controller.java
@@ -1,4 +1,17 @@
 package pe.edu.cibertec.DAWI_CL1_GRUPO2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Ejercicio5Controller {
+
+    @GetMapping("/ejercicio5")
+    public String form(Model model){
+        model.addAttribute("verresultado", false);
+        return "ejercicio5";
+
+    }
+
 }
