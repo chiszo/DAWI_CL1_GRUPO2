b02ae6f56f1cfacbabd09c7c4f74b06d1c2a12b6
maria309isabel <isa.mar.xv@gmail.com>
2023-09-30 11:54:36 -0500

    mensaje

--- src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO2/controller/Ejercicio5Controller.java
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO2/controller/Ejercicio5Controller.java
@@ -1,2 +1,4 @@
-package pe.edu.cibertec.DAWI_CL1_GRUPO2.controller;public class Ejercicio5Controller {
+package pe.edu.cibertec.DAWI_CL1_GRUPO2.controller;
+
+public class Ejercicio5Controller {
 }
--- src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO2/model/Ejercicio5.java
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO2/model/Ejercicio5.java
@@ -1,2 +1,4 @@
-package pe.edu.cibertec.DAWI_CL1_GRUPO2.model;public class Ejercicio5 {
+package pe.edu.cibertec.DAWI_CL1_GRUPO2.model;
+
+public class Ejercicio5 {
 }
