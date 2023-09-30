0139e138405ce48e73487db86e8b43b7bb6fab64
AlexGVP <i202122156@cibertec.edu.pe>
2023-09-30 05:53:51 -0500

    creando las paginas

---
+++ src/main/java/pe/edu/cibertec/DAWI_CL1_GRUPO2/model/Ejercicio3.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.DAWI_CL1_GRUPO2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ejercicio3 {
+    private Integer entero;
+}
